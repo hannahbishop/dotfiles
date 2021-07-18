8585f62dc802be92394fa3bb44a9b4090b8f02bf
Hannah Bishop <hbishop@forhims.com>
2021-07-18 15:56:14 -0700

    dot update

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -4,5 +4,14 @@ alias gfixup='findsha | xargs git commit --fixup'
 
 findsha()
 {
-  eval "git log --oneline | fzy | cut -c -9" 
+  git log --oneline | fzy | cut -c -9 
+};
+
+# Parameters
+# $1: commit message
+# gch // Git Commit Hers
+gch()
+{
+        MESSAGE=$(printf "[%s] - %s" $(grep -o "SXM-[0-9]*" <<< $(git rev-parse --abbrev-ref HEAD)) $1)
+        git commit -m $MESSAGE 
 };
